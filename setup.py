62433d3e6bf3e0dc4edc49dce1b61095bf4b610f
Rodney Baker <rod@rodneyjbaker.com>
2019-07-29 17:19:48 +0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To gather historical and ongoing rainfall data to use in the prediction of Australian wheat crop production.',
+    author='Rodney J Baker',
+    license='MIT',
+)
